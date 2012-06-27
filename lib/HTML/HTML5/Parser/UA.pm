e6bc4d9fec23d9209f297f49bf417fc3e6423a09
Toby Inkster <mail@tobyinkster.co.uk>
2012-06-27 15:46:38 +0100

    add docs
    --HG--
    branch : HTML-HTML5-Parser

--- lib/HTML/HTML5/Parser/UA.pm
+++ lib/HTML/HTML5/Parser/UA.pm
@@ -5,7 +5,7 @@ use strict;
 
 BEGIN {
 	$HTML::HTML5::Parser::UA::AUTHORITY = 'cpan:TOBYINK';
-	$HTML::HTML5::Parser::UA::VERSION   = '0.201_01';
+	$HTML::HTML5::Parser::UA::VERSION   = '0.201_02';
 }
 
 use Encode qw(decode);
@@ -140,5 +140,84 @@ sub _get_fs
 	};
 }
 
-
 1;
+
+=head1 NAME
+
+HTML::HTML5::Parser::UA - simple web user agent class
+
+=head1 DESCRIPTION
+
+This is a simple wrapper around HTTP::Tiny and LWP::UserAgent to smooth out
+the API differences between them. It only supports bog standard
+C<< get($url) >> requests.
+
+If LWP::UserAgent is already in memory, this module will use that.
+
+If LWP::UserAgent is not in memory, then this module will use HTTP::Tiny (or
+direct filesystem access for "file://" URLs).
+
+If LWP::UserAgent is not in memory, and you attempt to request a URL that
+HTTP::Tiny cannot handle (e.g. an "ftp://" URL), then this module will load
+LWP::UserAgent and die if it cannot be loaded (e.g. is not installed).
+
+HTML::HTML5::Parser::UA is used by the C<parse_file> method of
+HTML::HTML5::Parser.
+
+=head2 Class Method
+
+=over
+
+=item C<< get($url, $ua) >>
+
+Gets the URL and returns a hashref similar to HTTP::Tiny's hashrefs, but
+with an additional C<decoded_content> key, which contains the response
+body, decoded into a Perl character string (not a byte string).
+
+If $ua is given (it's optional), then this user agent will be used to
+perform the actual request. Must be undef or an LWP::UserAgent object
+(or a subclass) or an HTTP::Tiny object (or a subclass).
+
+=back
+
+=head2 Package Variable
+
+=over
+
+=item C<< $HTML::HTML5::Parser::NO_LWP >>
+
+If true, avoids using LWP::UserAgent.
+
+=back
+
+=head1 MOTIVATION
+
+L<LWP::UserAgent> is a good piece of software but it has a dependency on
+L<HTML::Parser>. L<HTML::Parser> is only used to provide one fairly
+esoteric feature, which this package doesn't make use of. (It's the
+C<parse_head> option.)
+
+Because of that, I don't especially want HTML::HTML5::Parser to have a
+dependency on LWP::UserAgent. Hence this module.
+
+=head1 SEE ALSO
+
+L<HTML::HTML5::Parser>.
+
+=head1 AUTHOR
+
+Toby Inkster, E<lt>tobyink@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Toby Inkster
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
