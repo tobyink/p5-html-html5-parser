27b59ba79cb85e732532b96101d7e1f661fb77eb
Toby Inkster <mail@tobyinkster.co.uk>
2012-06-27 15:28:56 +0100

    modernise docs
    --HG--
    branch : HTML-HTML5-Parser

--- lib/HTML/HTML5/Parser/Error.pm
+++ lib/HTML/HTML5/Parser/Error.pm
@@ -1,16 +1,10 @@
 package HTML::HTML5::Parser::Error;
 
-=head1 NAME
-
-HTML::HTML5::Parser::Error - an error that occured during parsing
-
-=cut
-
 use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = '0.201_01';
+our $VERSION = '0.201_02';
 
 use overload '""' => \&to_string;
 
@@ -19,37 +13,6 @@ sub new
 	my ($class, %args) = @_;
 	bless \%args, $class;
 }
-
-=head1 DESCRIPTION
-
-Note that L<HTML::HTML5::Parser> is not a validation tool, and there are many
-classes of error that it does not care about, so will not raise.
-
-The C<error_handler> and C<errors> methods of C<HTML::HTML5::Parser> generate
-C<HTML::HTML5::Parser::Error> objects.
-
-C<HTML::HTML5::Parser::Error> overloads stringification, so can be printed,
-matched against regular expressions, etc.
-
-=head2 Constructor
-
-=over
-
-=item C<< new(level=>$level, type=>$type, token=>$token, ...) >>
-
-Constructs a new C<HTML::HTML5::Parser::Error> object.
-
-=back
-
-=head2 Methods
-
-=over
-
-=item C<level>
-
-Returns the level of error. ('MUST', 'SHOULD', 'WARN', 'INFO' or undef.)
-
-=cut
 
 sub level
 {
@@ -63,23 +26,11 @@ sub level
 		}->{$self->{level}} || undef;
 }
 
-=item C<layer>
-
-Returns the parsing layer involved, often undef. e.g. 'encode'.
-
-=cut
-
 sub layer
 {
 	my $self = shift;
 	return $self->{layer} || undef;
 }
-
-=item C<type>
-
-Returns the type of error as a string.
-
-=cut
 
 sub type
 {
@@ -87,31 +38,12 @@ sub type
 	return $self->{type}||undef;
 }
 
-=item C<tag_name>
-
-Returns the tag name (if any).
-
-=cut
-
 sub tag_name
 {
 	my $self = shift;
 	return undef unless $self->{token} && exists $self->{token}{tag_name};
 	return $self->{token}{tag_name};
 }
-
-=item C<source_line>
-
-  ($line, $col) = $error->source_line();
-  $line = $error->source_line;
-  
-In scalar context, C<source_line> returns the line number of the
-source code that triggered the error.
-
-In list context, returns a line/column pair. (Tab characters count as
-one column, not eight.)
-
-=cut
 
 sub source_line
 {
@@ -126,12 +58,6 @@ sub source_line
 		return $self->{line};
 	}
 }
-
-=item C<to_string>
-
-Returns a friendly error string.
-
-=cut
 
 sub to_string
 {
@@ -160,6 +86,66 @@ sub to_string
 
 1;
 
+=head1 NAME
+
+HTML::HTML5::Parser::Error - an error that occured during parsing
+
+=head1 DESCRIPTION
+
+The C<error_handler> and C<errors> methods of C<HTML::HTML5::Parser> generate
+C<HTML::HTML5::Parser::Error> objects.
+
+C<HTML::HTML5::Parser::Error> overloads stringification, so can be printed,
+matched against regular expressions, etc.
+
+Note that L<HTML::HTML5::Parser> is not a validation tool, and there are many
+classes of error that it does not care about, so will not raise.
+
+=head2 Constructor
+
+=over
+
+=item C<< new(level=>$level, type=>$type, token=>$token, ...) >>
+
+Constructs a new C<HTML::HTML5::Parser::Error> object.
+
+=back
+
+=head2 Methods
+
+=over
+
+=item C<level>
+
+Returns the level of error. ('MUST', 'SHOULD', 'WARN', 'INFO' or undef.)
+
+=item C<layer>
+
+Returns the parsing layer involved, often undef. e.g. 'encode'.
+
+=item C<type>
+
+Returns the type of error as a string.
+
+=item C<tag_name>
+
+Returns the tag name (if any).
+
+=item C<source_line>
+
+  ($line, $col) = $error->source_line();
+  $line = $error->source_line;
+  
+In scalar context, C<source_line> returns the line number of the
+source code that triggered the error.
+
+In list context, returns a line/column pair. (Tab characters count as
+one column, not eight.)
+
+=item C<to_string>
+
+Returns a friendly error string.
+
 =back
 
 =head1 SEE ALSO
@@ -175,6 +161,11 @@ Toby Inkster, E<lt>tobyink@cpan.orgE<gt>
 Copyright (C) 2011-2012 by Toby Inkster
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.1 or,
-at your option, any later version of Perl 5 you may have available.
+it under the same terms as Perl itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
